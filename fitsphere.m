b832541b06ef88aafac8af69d5b76ab91a7e4265
NatashaTroxler <47993865+NatashaTroxler@users.noreply.github.com>
2020-11-12 17:46:59 -0800

    Updated to match points

--- fitsphere.m
+++ fitsphere.m
@@ -19,26 +19,52 @@ load('CameraCalibration/Canon/CanonParams.mat')
 canonCamParams = cameraParams;
 
 % Load the two actuator images from the different cameras
-nikon_img = imread('Pictures/FrontCam-Nikon/DSC_0370.JPG');
-canon_img = imread('Pictures/SideCam-Canon/IMG_0418.JPG');
+left_img = imread('Pictures/FrontCam-Nikon/Right1.JPG');
+right_img = imread('Pictures/FrontCam-Nikon/Left1.JPG');
 
 % Show undistorted images
 % figure
 % imshowpair(nikon_img, canon_img, 'montage'); 
 % title('Original Images');
 
-% nikon_img = undistortImage(nikon_img, nikonCamParams);
-% canon_img = undistortImage(canon_img, canonCamParams);
-% figure 
-% imshowpair(nikon_img, canon_img, 'montage');
-% title('Undistorted Images');
+left_img = undistortImage(left_img, nikonCamParams);
+right_img = undistortImage(right_img, nikonCamParams);
+figure 
+imshowpair(left_img, right_img, 'montage');
+title('Undistorted Images');
 
 % Detect feature points
-imagePoints1 = detectMinEigenFeatures(rgb2gray(nikon_img), 'MinQuality', 0.1);
+imagePoints1 = detectMinEigenFeatures(rgb2gray(left_img), 'MinQuality', 0.1);
+imagePoints2 = detectMinEigenFeatures(rgb2gray(right_img), 'MinQuality', 0.1);
 
 % Visualize detected points
 figure
+subplot(1,2,1)
 imshow(nikon_img, 'InitialMagnification', 50);
 title('150 Strongest Corners from the First Image');
 hold on
 plot(selectStrongest(imagePoints1, 150));
+
+% Visualize detected points
+subplot(1,2,2)
+imshow(canon_img, 'InitialMagnification', 50);
+title('150 Strongest Corners from the Second Image');
+hold on
+plot(selectStrongest(imagePoints2, 150));
+
+%Track points
+tracker = vision.PointTracker('MaxBidirectionalError', 1, 'NumPyramidLevels', 5);
+
+% Initialize the point tracker
+imagePoints1 = imagePoints1.Location;
+initialize(tracker, imagePoints1, left_img);
+
+% Track the points
+[imagePoints2, validIdx] = step(tracker, right_img);
+matchedPoints1 = imagePoints1(validIdx, :);
+matchedPoints2 = imagePoints2(validIdx, :);
+
+% Visualize correspondences
+figure
+showMatchedFeatures(left_img, right_img, matchedPoints1, matchedPoints2);
+title('Tracked Features');
